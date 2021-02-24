164d5961f5ea37f606a0868bf662e1d3fbbac28d
isox <theo.meunier41@gmail.com>
2021-02-24 23:28:10 +0100

    feat: mise en place de l'animation pour le formulaire de contact

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,26 @@ import './scss/style.scss';
 import $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
+
+//on recupere l'element
+let btn = document.getElementById('btn-contact')
+let form = document.getElementById('contactForm')
+
+//on verifie si il a la classe active_btn
+
+//on detecte le clique du bouton
+btn.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (form.classList.contains('active_btn')) {
+        //on surpprime la class
+        form.classList.remove("active_btn");
+        //on ajoute une class
+        form.classList.add("cache_form")
+    } else {
+        //on surpprime la class
+        form.classList.remove("cache_form");
+        //on ajoute une class
+        form.classList.add("active_btn")
+    }
+})
+
